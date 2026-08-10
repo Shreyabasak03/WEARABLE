3080bf75b76335c65e7f4b49eec8dd5ee7a0ad4e
Shreya Basak <shreyabasak36@gmail.com>
2026-08-10 13:23:45 +0530

    image update

--- src/pages/Women.jsx
+++ src/pages/Women.jsx
@@ -69,8 +69,8 @@ useEffect(() => {
 
         <div className="image">
           <img
-            src="https://images.unsplash.com/photo-1516826957135-700dedea698c?q=80&w=1600&auto=format&fit=crop"
-            alt="Men's Fashion"
+            src="https://images.unsplash.com/photo-1558769132-cb1aea458c5e?q=80&w=1674&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Women's Fashion"
           />
         </div>
 
