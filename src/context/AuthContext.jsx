8161f408a9530cdc2a86791ae0e9b52a6332a7c7
Shreya Basak <shreyabasak36@gmail.com>
2026-08-23 20:45:54 +0530

    update

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,7 +5,7 @@ import React, {
   useState,
 } from "react";
 
-import api from "../api/api"; // Adjust import path if needed
+import api from "../api/Axios"; // Adjust import path if needed
 
 const AuthContext = createContext();
 
